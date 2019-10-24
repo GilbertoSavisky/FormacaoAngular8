0c63dfa810c365594cedb31fcbc4dbe81cadecdc
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-10-24 14:54:39 -0300

    24. Criando o validador de CPF

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 import { CadastroPj } from '../../models/cadastro-pj-models';
+import { CpfValidator } from 'src/app/shared/validators/cpf.validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -26,7 +27,7 @@ export class CadastrarPjComponent implements OnInit {
       nome: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(6)]],
-      cpf: ['', [Validators.required]],
+      cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
       cnpj: ['', [Validators.required]]
     });
--- src/app/shared/directives/mascara.directive.ts
+++ src/app/shared/directives/mascara.directive.ts
@@ -1,5 +1,12 @@
-import { Directive, Input, ElementRef, HostListener } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+import {
+  Directive,
+  HostListener,
+  Input,
+  ElementRef
+} from '@angular/core';
+import {
+  NG_VALUE_ACCESSOR, ControlValueAccessor
+} from '@angular/forms';
 
 @Directive({
   selector: '[mascara]',
@@ -27,23 +34,26 @@ export class MascaraDirective implements ControlValueAccessor {
   registerOnChange(fn: any) {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any) {
-    this.onChange = fn;
+    this.onTouched = fn;
   }
 
   @HostListener('keyup', ['$event'])
   onKeyup($event: any) {
     let valor: string = $event.target.value.replace(/\D/g, '');
 
-    // retorna casa pressionado backspace
+    // retorna caso pressionado backspace
     if ($event.keyCode === 8) {
       this.onChange(valor);
       return;
     }
+
     let pad = this.mascara.replace(/\D/g, '').replace(/9/g, '_');
     if (valor.length <= pad.length) {
       this.onChange(valor);
     }
+
     $event.target.value = this.aplicarMascara(valor);
   }
 
@@ -70,11 +80,16 @@ export class MascaraDirective implements ControlValueAccessor {
         valor += valorMask[valorMaskPos++];
       }
     }
+
     if (valor.indexOf('_') > -1) {
       valor = valor.substr(0, valor.indexOf('_'));
     }
-    return valor;
 
+    return valor;
   }
 
 }
+
+
+
+
