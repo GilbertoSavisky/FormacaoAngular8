e8c8717d4bdceaaf5757f2bc9a0d998afd5bedfe
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-10-25 14:14:22 -0300

    33. Criando o modulo e componente do painel de funcionarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatIconModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { 
@@ -14,6 +17,7 @@ import {
   CadastroPfRoutingModule
 } from './autenticacao';
 import { AppRoutingModule } from './app-routing.module';
+import { FuncionarioModule } from './funcionario';
 
 @NgModule({
   declarations: [
@@ -22,13 +26,16 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
     LoginModule,
     LoginRoutingModule,
     CadastroPjModule,
     CadastroPjRoutingModule,
     CadastroPfModule,
     CadastroPfRoutingModule,
+    FuncionarioModule,
 
     AppRoutingModule
   ],
---
+++ src/app/funcionario/funcionario.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListagemComponent, LancamentoComponent } from './components';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  	ListagemComponent, 
+  	LancamentoComponent
+  ]
+})
+export class FuncionarioModule { }
