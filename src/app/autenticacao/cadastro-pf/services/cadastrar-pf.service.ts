9d3c8a1d8e7d5d56dcbd67b60a214981c3ebe1c0
GILBERTO SAVISKY <gsavisky@ad.mppr.mp.br>
2019-10-28 14:41:29 -0300

    40-gravando-lancamento-componente

--- src/app/autenticacao/cadastro-pf/services/cadastrar-pf.service.ts
+++ src/app/autenticacao/cadastro-pf/services/cadastrar-pf.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { environment as env } from '../../../../environments/environment';
-import { CadastroPf } from '../models';
 
+import { CadastroPf } from '../';
 
 @Injectable()
 export class CadastrarPfService {
